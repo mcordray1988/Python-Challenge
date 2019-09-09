a0abc7127e4705148735b9376704315960f645cf
Morgan <mcordray88@protonmail.com>
2019-09-09 15:03:37 -0700

    Create main.py

---
+++ PyPoll/main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+df = pd.read_csv('/Users/morgancordray/Desktop/03-Python/Instructions/PyPoll/Resources/election_data.csv')
+voteTotal = df['Voter ID'].count()
+Khan = df[df['Candidate'] == "Khan"]
+Correy = df[df['Candidate'] == "Correy"]
+Li = df[df['Candidate'] == "Li"]
+OTooley = df[df['Candidate'] == "O\'Tooley"]
+print("Election Results")
+print("------------------------")
+print(f"Total Votes: {voteTotal}")
+print("------------------------")
+print(f"Khan: {round((Khan.Candidate.count() / voteTotal)*100, 6)}% ({Khan.Candidate.count()})")
+print(f"Correy: {round((Correy.Candidate.count() / voteTotal)*100, 6)}% ({Correy.Candidate.count()})")
+print(f"Li: {round((Li.Candidate.count() / voteTotal)*100, 6)}% ({Li.Candidate.count()})")
+print(f"O\'Tooley: {round((OTooley.Candidate.count() / voteTotal)*100, 6)}% ({OTooley.Candidate.count()})")
+print("------------------------")
+winner = pd.DataFrame(df['Candidate'].value_counts())
+winner['Name'] = winner.index
+print(winner.head())
+
+k = winner.loc[winner['Candidate'] == winner['Candidate'].max()]
+print (f"WINNER : {k['Name']}")
